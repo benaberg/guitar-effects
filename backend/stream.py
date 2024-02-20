d87724162cc5dbff10ba7a8ceddf84b9c6037e9f
Benjamin Åberg <benjamin.aaberg@outlook.com>
2024-02-20 15:34:50 +0200

    Moved stream parameters to class init

--- backend/stream.py
+++ backend/stream.py
@@ -3,30 +3,57 @@ import argparse
 import numpy
 assert numpy
 
-CHANNELS = 2
-RATE = 48000
-BLOCK = 1024
-LATENCY = 0
-INPUT_DEVICE = 20
-OUTPUT_DEVICE = 20
-
-class AudioStream():
-    def __init__(self):
+class AudioStream(object):
+    def __init__(self, rate, block, channels, latency, input_device, output_device):
         self.parser = argparse.ArgumentParser(add_help=False)
+        self.rate = rate
+        self.block = block
+        self.channels = channels
+        self.latency = latency
+        self.input_device = input_device
+        self.output_device = output_device
+        print(sd.query_devices())
+
+    def echo(seldf, indata, rate=192000, chunk=1024, delay=0.5, decay=0.5):
+        
+        indata = indata[:, 0]
+        
+        # Calculate delay in samples
+        delay_samples = int(delay * 192000)
+
+        # Initialize output array with zeros
+        outdata = numpy.zeros_like(indata)
+
+        # Iterate over the input signal in chunks of buffer_size
+        for i in range(0, len(indata), chunk):
+            # Extract a chunk of the input signal
+            chunk = indata[i:i + chunk]
+
+            # Create a delayed version of the chunk
+            delayed_chunk = numpy.concatenate([numpy.zeros(delay_samples), chunk])
+
+            # Apply decay to the delayed chunk
+            decayed_chunk = delayed_chunk * decay
+
+            # Add the decayed chunk to the output signal
+            outdata[i:i + len(chunk)] += decayed_chunk
+
+        return outdata
 
     def callback(self, indata, outdata, frames, time, status):
         if status:
             print(status)
         
         outdata[:] = indata
+        #outdata[:] = self.echo(indata)
 
     def stream(self):
         try :
-            with sd.Stream(device=(INPUT_DEVICE, OUTPUT_DEVICE), 
-                        samplerate=RATE, 
-                        blocksize=BLOCK, 
-                        latency=LATENCY, 
-                        channels=CHANNELS, 
+            with sd.Stream(device=(self.input_device, self.output_device), 
+                        samplerate=self.rate, 
+                        blocksize=self.block, 
+                        latency=self.latency, 
+                        channels=self.channels, 
                         callback=self.callback):
                 print('press ENTER to quit')
                 input()
--- backend/test.py
+++ backend/test.py
@@ -1,4 +1,4 @@
 import stream
 
-audioStream = stream.AudioStream()
+audioStream = stream.AudioStream(96000, 128, 2, 0, 20, 20)
 audioStream.stream()
